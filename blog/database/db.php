cf8dc624f66be0a4dae3db3749da4c83d145901f
avinash <sonukumer4596@gmail.com>
2023-11-02 02:53:22 +0530

    main

---
+++ blog/database/db.php
@@ -0,0 +1,6 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+	$database_username = 'root';//user name
+	$database_password = '';//password
+	$pdo_conn = new PDO( 'mysql:host=localhost;dbname=blog_admin_db', $database_username, $database_password );
+?>
